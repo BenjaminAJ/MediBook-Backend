0658763573c8ab47aba670a4a77ba72c84edbac4
Benjamin <ajanakub30@gmail.com>
2025-06-29 18:44:20 +0100

    feat: Adds `jsonwebtoken` and `cors` as new dependencies.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Enable CORS for all origins
+app.use(cors({origins: '*', credentials: true}));
 
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
